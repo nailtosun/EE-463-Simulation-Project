1d340d26100e5b97694c45b74b85df8cfdefca79
DESKTOP-6IJJOUF\nailt <nailtosun07@gmail.com>
2018-11-11 19:28:08 +0300

    Question 2 simulink file added, Q2 plots are added.

--- Q1_plotting_codes.m
+++ Q1_plotting_codes.m
@@ -8,3 +8,15 @@ title('Output voltage of single phase rectifier with different sampling time')
 xlabel('Time (secs)')
 ylabel('Voltage (Volts)')
 legend('1 usec', '10 usec', '1.5 ms')
+
+%% Question 2 plotting codes
+plot(milihenry);
+grid on;
+hold on;
+plot(tenmilihenry)
+plot(onehenry)
+plot(onehenrywithline)
+legend('1 mH','10 mH','1 H','1H with Ls=10 mH')
+title('Output voltage of single phase rectifier with different inductance values')
+xlabel('Time (secs)')
+ylabel('Voltage (Volts)')
